2e5cef62271cb9623384bf46bda1ff4a461cb021
Dilum paranavithana <dilumchandupa@gmail.com>
2024-10-16 15:38:39 +0530

    delete product from HTML when clicking delete

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -21,7 +21,7 @@ cart.forEach((cartItem) => {
 
     if (matchingProduct) {
         cartSummaryHTML += `
-         <div class="cart-item-container">
+         <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Wednesday, June 15
             </div>
@@ -90,5 +90,9 @@ document.querySelectorAll('.js-delete-link')
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
+
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
+
     });
 });
