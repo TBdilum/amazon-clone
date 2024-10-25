6f0e3848c0f634a9a25ddbc63b4d808164575385
Dilum paranavithana <dilumchandupa@gmail.com>
2024-10-25 14:25:37 +0530

    convert products into classes

---
+++ javascript-amazon-project-main/data/cart-oop.js
@@ -0,0 +1,68 @@
+import { deliveryOptions } from "./delivery.js";
+
+
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage(){
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+
+    if (!cart) {
+        cart = [{
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2,
+            deliveryOptionsId: '1'
+        }, {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1,
+            deliveryOptionsId: '2'
+        }];
+    }
+}
+
+
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+    const matchingItem = cart.find(cartItem => cartItem.productId === productId);
+
+    if (matchingItem) {
+        matchingItem.quantity += 1; // Increment existing item's quantity
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1, // Add new item with quantity 1
+            deliveryOptionsId: '1'
+        });
+    }
+
+    saveToStorage();
+}
+
+export function removeFromCart(productId) {
+    cart = cart.filter(cartItem => cartItem.productId !== productId);
+    saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionsId) {
+    const matchingItem = cart.find(cartItem => cartItem.productId === productId);
+
+    if (matchingItem) {
+        // Check if the provided deliveryOptionsId exists
+        const optionExists = deliveryOptions.some(option => option.id === deliveryOptionsId);
+
+        if (optionExists) {
+            matchingItem.deliveryOptionsId = deliveryOptionsId;
+            saveToStorage();
+        } else {
+            console.error(`Delivery option ${deliveryOptionsId} does not exist.`);
+        }
+    } else {
+        console.error(`Product ${productId} not found in the cart.`);
+    }
+};
---
+++ javascript-amazon-project-main/tests-jasmine/data/cartTest.js
@@ -0,0 +1,11 @@
+import { addToCart } from "../../data/cart.js";
+
+describe('test suite: addToCart', () => {
+    it('adds an exsisting product to the cart' , () => {
+
+    });
+
+    it('adds a new product to the cart' , () => {
+        addToCart( )
+    });
+});
