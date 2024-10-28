22c21933f7968b3e1cf3c4288e6dc81b72ed0a3f
Dilum paranavithana <dilumchandupa@gmail.com>
2024-10-28 14:52:14 +0530

    use inheritnce and polymorphism to add size chart

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -29,13 +29,31 @@ class Product {
 
   getStarsUrl () {
     return `images/ratings/rating-${this.rating.stars * 10}.png`
-  }
+  };
 
   getPrice () {
-    return `${(this.priceCents / 100).toFixed(2) }`
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+
+  extraInfoHTML() {
+    return '';
   }
 };
 
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+   super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    super.extraInfoHTML();
+    return `
+    <a href="${this.sizeChartLink}" target = '_blank'>Size Chart</a>`
+  }
+};
 
 export const products = [
   {
@@ -697,5 +715,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing'){
+    return new Clothing(productDetails)
+  }
   return new Product(productDetails);
 });
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -36,7 +36,7 @@ products.forEach((product) => {
                 </div>
 
                 <div class="product-price">
-                    $${product.getPrice()}
+                    ${product.getPrice()}
                 </div>
 
                 <div class="product-quantity-container">
@@ -53,6 +53,8 @@ products.forEach((product) => {
                     <option value="10">10</option>
                     </select>
                 </div>
+
+                ${product.extraInfoHTML()}
 
                 <div class="product-spacer"></div>
 
